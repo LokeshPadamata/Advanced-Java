f7a5b54d9a761b748739f418bbae8f5b3785ef9c
LokeshPadamata <127922284+LokeshPadamata@users.noreply.github.com>
2023-05-01 12:03:19 +0530

    Mini Project

---
+++ ProductionManagementSystem/src/ProductionManagementSystem.java
@@ -0,0 +1,68 @@
+package ProductionManagementSystem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class ProductionManagementSystem {
+	static Connection connection = null;
+	static Statement statement = null;
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		ProductionManagementSystem.getDataBaseConnection();
+		ProductionManagementSystem.CreateTable();
+		addProduct(101,"'watch'",2000);
+		addProduct(102,"'Mobile'",20000);
+		addProduct(103,"'Laptop'",50000);
+		updateProduct(102,"SmartPhone");
+		closeConnection();
+		
+		deleteProduct(103);
+		deleteProduct(102);
+		deleteProduct(101);
+	 }
+	 static void getDataBaseConnection() throws ClassNotFoundException, SQLException {
+		// load the driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		// get connection
+		ProductionManagementSystem.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ProductManagementSystem","root","Lokesh@123");
+		System.out.println(connection);
+		statement = connection.createStatement();		
+	 }
+	 static void CreateTable() throws SQLException {
+		String query = "create table Products(productId int,productName varchar(50),productPrice int)";
+		System.out.println(query);
+		statement.execute(query);
+		System.out.println("Table Created");
+	}
+	static void addProduct(int id,String name,int price) throws SQLException {
+		String query = "insert into products values("+id+","+name+","+price+")";
+		System.out.println(query);
+		statement.execute(query);
+		System.out.println("inserted");
+	 }
+	
+	 static void deleteProduct(int id) throws SQLException {
+		String query = "delete from Products where productId="+id;
+		statement.execute(query);
+		System.out.println("Product deleted");
+	 }
+	 static void updateProduct(int id,String name) throws SQLException  {
+		if(name!=null) {
+			String query = "update Products set productName='"+name+"' where productId="+id;
+			statement.execute(query);
+			System.out.println("Updated");
+		}
+	  }	
+		static void getAllProducts() throws SQLException{
+			ResultSet rs = statement.executeQuery("Select * from Products");
+			System.out.println("productId   \t ProductName     \t  ProductPrice");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1)+"     \t" +rs.getString(2)+" \t     "+rs.getString(3));
+			}		
+		}	
+		static void closeConnection() throws SQLException {
+			connection.close();
+			System.out.println("Connection closed");
+		}
+
+	}
