ac835c6e7da41b1f1e1a6b466b97475d2260e528
LokeshPadamata <127922284+LokeshPadamata@users.noreply.github.com>
2023-05-03 22:08:12 +0530

    Session Management

---
+++ SessionManagement/src/main/java/com/httpsession/Servlet.java
@@ -0,0 +1,37 @@
+package com.httpsession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Servlet
+ */
+ //@WebServlet("/servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Servlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Servlet1 Executed");
+		HttpSession session = request.getSession();
+		session.setAttribute("username","Lokesh");
+		PrintWriter out = response.getWriter();
+		out.println("<a href='./servlet2'>servlet2</a>");
+
+	}
+
+}
