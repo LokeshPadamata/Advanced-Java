3fb9720b172bf0f551b03d8f0ea1f5b86d4e4b9a
LokeshPadamata <127922284+LokeshPadamata@users.noreply.github.com>
2023-06-03 15:41:42 +0530

    SpringBoot Project

---
+++ DigiKart/src/main/java/com/ecommerce/digikart/DigiKartApp.java
@@ -0,0 +1,12 @@
+package com.ecommerce.digikart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiKartApp {
+	public static void main(String[] args) {
+		SpringApplication.run(DigiKartApp.class, args);
+		
+	}
+}
