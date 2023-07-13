60297dd87e3898049e4bb0817111cd587b647a33
LokeshPadamata <127922284+LokeshPadamata@users.noreply.github.com>
2023-07-13 16:14:47 +0530

    Delete Book Store-SpringBoot-mongodb directory

--- Book Store-SpringBoot-mongodb/src/main/java/com/springboot/mongo/api/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.mongo.api.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.springboot.mongo.api.model.Book;
-
-public interface BookRepository extends MongoRepository<Book, Integer> {
-
-}
